e1ffae532e7be721378c917ab2bd8979e3c22259
rob-bee-neilson <robbie.neilson@gmail.com>
2019-02-25 15:49:19 +1300

    dependencies

---
+++ server/server.js
@@ -0,0 +1,13 @@
+
+const path = require('path')
+const express = require('express')
+
+//const todos = require('./routes/todos')
+
+const server = express()
+
+server.use(express.static(path.join(__dirname, 'public')))
+
+//server.use('/api/v1/todos', todos)
+
+module.exports = server

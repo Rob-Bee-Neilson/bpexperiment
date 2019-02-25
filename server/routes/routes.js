ef99572721aa71f545bdb8dc8513304d82b385c9
rob-bee-neilson <robbie.neilson@gmail.com>
2019-02-25 16:56:19 +1300

    migrations and seeds

---
+++ server/routes/routes.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const router = express.Router()
+const db = require('../db/db')
+
+router.use(express.json())
+
+router.get('/', (req, res) => {
+  db.getAllTodos()
+  .then(todos => {
+    res.json(todos)
+  })
+})
+
+module.exports = router
